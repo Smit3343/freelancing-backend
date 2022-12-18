62f55ed753127d845e740639c0ecfeface8aefa3
unknown <smitvaghani3343@gmail.com>
2022-12-18 11:35:47 +0530

    logging added

--- db.js
+++ db.js
@@ -2,14 +2,13 @@ const moongoose = require("mongoose");
 const config = require('./config');
 
 const connectToMongoDb = () => {
-    console.log(config.MONGO_URI);
-    moongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error) => {
-        if (error) {
-            console.error(error.message);
-        }
-        else {
-            console.log("mongodb connected  successfully");
-        }
-    })
+    moongoose.connect(config.MONGO_URI,
+        {
+            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+        }).then(() => {
+            console.log("MongoDb Connected Sucessfully!");
+        }).catch((err) => {
+            console.log(err);
+        })
 }
 module.exports = connectToMongoDb;
